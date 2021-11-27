bd1941945d55f3f3201f24a6d180bb4675e737cf
Eugene D <derepa20@gmail.com>
2021-11-27 21:12:09 +0200

    Code before changing login logic

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
@@ -43,6 +44,6 @@ export class AccountService {
 
   logout() {
     localStorage.removeItem('user');
-    this.currentUserSource.next(null as any);
+    this.currentUserSource.next(null!);
   }
 }
